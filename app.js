d99e5fb9427a9d85b5485fbabebdbd8765cc5b87
KABANOFESTO <kabanofesto1@gmail.com>
2024-06-22 16:34:17 +0200

    ch-fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3004;
 
 app.listen(PORT, () => console.log(`running on localhost:${PORT}`));
 
